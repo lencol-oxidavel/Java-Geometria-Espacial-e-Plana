adc8775a80c0a5d32b28cb9648fe9ed3b8db11f1
Marcelo Corrêa Padilha <108826467+lencol-oxidavel@users.noreply.github.com>
2025-08-13 10:22:30 -0300

    hmmm

---
+++ Aplicacao.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Aplicacao {
+    public static void main(String[] args){
+        int escolha;
+        Scanner ler = new Scanner(System.in);
+
+        while(true){
+            // menu
+            System.out.println("===== MENU PRINCIPAL =====");
+            System.out.println("1 - Geometria Plana \n2 - Geometria Espacial \n3 - Sair");
+            escolha = ler.nextInt();
+
+            
+
+        }
+
+    }
+}
--- classes/GeometriaPlana.java
+++ classes/GeometriaPlana.java
@@ -1,6 +1,38 @@
 package classes;
 
 public class GeometriaPlana {
+
+    // prisma
+    public double prismaAreaLateral(int n_De_lados, double lado1, double altura){
+        return (lado1 * altura) * n_De_lados;
+    }
+    public double prismaAreaBase(int n_De_lados, double lado1, double apotema){
+        return (n_De_lados * lado1 * apotema) / 2;
+    }
+    public double prismaAreaTotal(double prismaAreaBase, double prismaAreaLateral){
+        return prismaAreaBase + prismaAreaLateral;
+    }
+    public double prismaVolume(double prismaAreaBase, double altura){
+        return prismaAreaBase * altura;
+    }
+    // prisma
+
+    // cilindro
+    public double cilindroAreaBase(double raio){
+        return Math.PI * Math.pow(raio, 2);
+    }
+    public double cilindroAreaLateral(double raio, double altura){
+        return (2 * Math.PI) * raio * altura;
+    }
+    public double cilindroVolume(double raio, double altura){
+        return Math.PI * Math.pow(raio, 2) * altura;
+    }
+    // cilindro 
     
+    // pirâmide
+    
+    // pirâmide
+
+
 
 }
