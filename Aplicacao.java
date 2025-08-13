a6b3d315a30b2765c12e9c35e0526aadb1cdb6c8
Marcelo Corrêa Padilha <marcelocpadilha51@gmail.com>
2025-08-13 14:51:43 -0300

    Update Aplicacao.java

--- Aplicacao.java
+++ Aplicacao.java
@@ -9,6 +9,8 @@ public class Aplicacao {
             // menu
             System.out.println("===== MENU PRINCIPAL =====");
             System.out.println("1 - Geometria Plana \n2 - Geometria Espacial \n3 - Sair");
+            System.out.println("==========================");
+            System.out.println("Escolha: ");
             escolha = ler.nextInt();
 
             
